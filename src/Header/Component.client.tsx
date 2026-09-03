030b8beb5c0e190430b46a79e4e296a7457de042
narcalli <164997672+narcalli@users.noreply.github.com>
2026-09-03 18:12:44 +0530

    Fix header component layout and theme handling

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -29,14 +29,15 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [headerTheme])
 
-  return (
+    return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+      <div className="py-4 flex justify-between items-center">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high" />
         </Link>
         <HeaderNav data={data} />
       </div>
     </header>
+  )
   )
 }
