0190779a1b299c4282bae89bab990f1c3e6f4688
narcalli <164997672+narcalli@users.noreply.github.com>
2026-09-07 17:28:45 +0530

    Change logo text color to currentColor
    Updated logo text color to use currentColor.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -19,7 +19,7 @@ export const Logo = (props: Props) => {
         @import url('https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,700&display=swap');
         .ncx-logo{display:inline-flex;align-items:center;gap:10px;
           font-family:"Bricolage Grotesque",system-ui,sans-serif;font-weight:700;
-          font-size:21px;letter-spacing:-.025em;color:#16203A;line-height:1}
+          font-size:21px;letter-spacing:-.025em;color:currentColor;line-height:1}
         .ncx-logo img{width:auto;height:32px;display:block}
       `}</style>
       {/* eslint-disable-next-line @next/next/no-img-element */}
